e0f83455597ec252d01921cef8f7c109002786e7
William Bainton <baintonw@gmail.com>
2019-05-03 03:15:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,10 +27,11 @@ def using_concat(array, other_array)
 end
 
 def using_insert(array, element)
-  array.insert(3, element)
+  array.insert(4, element)
 end
 
 def using_uniq(array)
+  
 end
 
 def using_flatten(array)
